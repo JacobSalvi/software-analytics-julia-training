55442144cb744305ad1fcbee503de096eec9b40f
JacobSalvi <43408191+JacobSalvi@users.noreply.github.com>
2024-12-01 11:47:16 +0100

    Preprocessing

--- src/utils/preprocess.py
+++ src/utils/preprocess.py
@@ -1,14 +1,26 @@
 import pandas as pd
 
+from src.utils import util
+
 
 def preprocess(df: pd.DataFrame) -> pd.DataFrame:
-    def function_body_is_dummy(el):
-        if el["doc_string"] != "":
-            return el["function_body"]
-        if el["has_comments"] or el["has_constraints"]:
-            return el["function_body"]
-        return ""
-    df.loc[(df["doc_string"] == "") & (df["has_internal_comments"] == False) & (df["has_constraints"]==False), "function_body"] = ""
-    df = df[df.apply(function_body_is_dummy, axis=1)]
-    pass
-    # a = df.loc[lambda el: ]
+    df.loc[(df["doc_string"] == "") & (df["has_internal_comments"] == False) & (df["has_constraints"] == False), "function_body"] = ""
+    df['function_body'] = df['function_body'].str.replace(r'(^|\n).*# TODO.*(\n|$)', '', regex=True)
+    benchmark = pd.read_json(util.benchmark_prompts(), lines=True)
+    benchmark['function_name'] = benchmark['prompt'].str.split('\n').str[-2]
+    benchmark["function_name"] = benchmark["function_name"].str.split('function ').str[-1].str.split("(").str[0]
+    function_names_filter = benchmark["function_name"].tolist()
+    mask = df['function_header'].apply(
+        lambda x: any(name == x.split("function ")[-1].split("(")[0] for name in function_names_filter))
+    df = df[~mask]
+    return df
+
+
+def main():
+    df = pd.read_json(util.data_dir().joinpath("function_definitions.json"), lines=True)
+    df = preprocess(df)
+    df.to_json(util.data_dir().joinpath("function_definitions_preprocessed.json"), lines=True)
+
+
+if __name__ == '__main__':
+    main()
--- src/utils/util.py
+++ src/utils/util.py
@@ -12,6 +12,10 @@ def repositories_json() -> Path:
     return Path(__file__).parents[2].joinpath('repositories.json')
 
 
+def benchmark_prompts() -> Path:
+    return data_dir().parent.joinpath('benchmark_prompts.jsonl')
+
+
 def get_repository_urls() -> pd.DataFrame:
     repo_json: Path = repositories_json()
     repositories = json.load(repo_json.open())
